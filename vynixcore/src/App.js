1c023a7f43b12280b525718bbb1d9b0b5fa8d765
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2025-08-26 20:13:34 +0600

    feat: Create Contact component and integrate into App.js

--- vynixcore/src/App.js
+++ vynixcore/src/App.js
@@ -4,6 +4,7 @@ import Hero from './components/Hero';
 import Services from './components/Services';
 import Portfolio from './components/Portfolio';
 import Blog from './components/Blog';
+import Contact from './components/Contact';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Services />
         <Portfolio />
         <Blog />
+        <Contact />
       </main>
     </div>
   );
