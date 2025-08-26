f7f5143fd4c90db40c30c5ad5db8bbf141f8b754
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2025-08-26 22:04:13 +0600

    fix: Explicitly import postcss.config.js in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import postcssConfig from './postcss.config.js'; // Import postcss.config.js
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: postcssConfig, // Use the imported postcssConfig
+  },
 })
