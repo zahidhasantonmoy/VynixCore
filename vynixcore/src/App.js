22c0ee9e566e03916a781546a639e055acebbfb5
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2025-08-26 20:11:57 +0600

    feat: Create Services component and integrate into App.js

--- vynixcore/src/App.js
+++ vynixcore/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
+import Services from './components/Services';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <main>
         <Hero />
+        <Services />
       </main>
     </div>
   );
