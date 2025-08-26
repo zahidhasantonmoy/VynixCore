7215a60a36915305cb52c0e41fb0d59d416e9ddb
Zahid Hasan Tonmoy <zahidhasantonmoy360@gmail.com>
2025-08-26 20:12:26 +0600

    feat: Create Portfolio component and integrate into App.js

--- vynixcore/src/App.js
+++ vynixcore/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Services from './components/Services';
+import Portfolio from './components/Portfolio';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <main>
         <Hero />
         <Services />
+        <Portfolio />
       </main>
     </div>
   );
